77fe352e138f702c417066371b0099902614c1d4
anis ayed <ayed.aniss@hotmail.com>
2024-04-03 01:37:27 +0200

    Feature dynamic form with json constant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {DynamicFormComponent} from "./dynamic-form/dynamic-form.component";
+import {registerFormConfig} from "./constants/register-form.config";
+import {IForm} from "./models/form.interface";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DynamicFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'dynamic-forms-json';
+  readonly registerForm: IForm = registerFormConfig;
 }
